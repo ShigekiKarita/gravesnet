7b2cbe50bcccb434a734e5cea4d119a5e68ffde3
karita@home <shigekikarita@gmail.com>
2015-07-28 21:08:13 +0900

    add travis

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
     name = 'gravesnet',
     version = '0.1',
     description='This is test codes for travis ci',
-    packages = find_packages()
+    packages = find_packages(),
+    tests_require=['nose'],
 )
